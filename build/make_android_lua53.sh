876ad330d5d815c134c62061e3ed4cb1e838c672
huangxuyang <forsakenyang@vip.qq.com>
2020-12-14 15:11:44 +0800

    Revert "up"
    This reverts commit 1b02938463ae0a90a31470b8d496e7f8d2c6e079.
    
    Revert "使用老版的NDK"
    
    This reverts commit a0db8bfc7a84d013f6ded8f36227daf7ac3de3d3.

--- build/make_android_lua53.sh
+++ build/make_android_lua53.sh
@@ -5,7 +5,7 @@ elif [ -n "$ANDROID_NDK_HOME" ]; then
 elif [ -n "$ANDROID_NDK_HOME" ]; then
     export NDK=${ANDROID_NDK_HOME}
 else
-    export NDK=~/android-ndk-r10e
+    export NDK=~/android-ndk-r15c
 fi
 
 if [ ! -d "$NDK" ]; then
